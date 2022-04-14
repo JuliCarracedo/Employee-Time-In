b9ff58f916c59d53e5bbef14553dcb47417362f4
JuliCarracedo <julicarra@hotmail.com>
2022-04-14 12:35:44 -0300

    Allow timeframes overlapping

--- app/Employee.rb
+++ app/Employee.rb
@@ -7,9 +7,45 @@ class Employee
         @times = []
     end
 
-    def insert_times timeIn, timeOut
-        @timeIn = MyTime.new(timeIn)
-        @timeOut = MyTime.new(timeOut)
-        @times << [@timeIn, @timeOut]
+    def insert_times time_in, time_out
+        @time_in = MyTime.new(time_in)
+        @time_out = MyTime.new(time_out)
+
+        #Check that times are correctly located
+        if @time_in.later_than?(@time_out)
+            return nil
+        end
+
+        if self.merge([@time_in, @time_out]) || self.contains?([@time_in, @time_out])
+            return nil
+        end
+
+        @times << [@time_in, @time_out]
+    end
+
+    private 
+
+    def contains? new_times
+        @times.each do |t1|
+            if new_times[0].later_than?(t1[0]) && t1[1].later_than?(new_times[1])
+                return true
+            end
+        end
+        return false
+    end
+
+    def merge new_times
+        @change = false
+        @times.each do |t1|
+            if t1[0].later_than?(new_times[0]) && new_times[1].later_than?(t1[0])
+                t1[0] = new_times[0]
+                @change = true
+            end
+            if t1[1].later_than?(new_times[0]) && new_times[1].later_than?(t1[1])
+                t1[1] = new_times[1]
+                @change = true
+            end           
+        end
+        return @change
     end
 end
--- app/script.rb
+++ app/script.rb
@@ -2,7 +2,12 @@ require_relative "Employee"
 
 u = Employee.new("Evan")
 
-u.insert_times("10:00","11:00")
+u.insert_times("10:00","13:00")
+u.insert_times("11:00","12:00")
+u.insert_times("09:00","12:00")
+u.insert_times("10:00","14:00")
+u.insert_times("08:00","15:00")
 u.times.each do |t|
-   puts t[0].later_than? t[1]
+   puts t[0].time
+   puts t[1].time
 end
