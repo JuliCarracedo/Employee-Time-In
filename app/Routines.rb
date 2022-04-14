458da2fb76cdec4aadb4537d072cdb1db6ad1388
JuliCarracedo <julicarra@hotmail.com>
2022-04-14 13:31:28 -0300

    In time loop

--- app/Routines.rb
+++ app/Routines.rb
@@ -3,7 +3,7 @@ require_relative "./MyTime"
 
 module Routines
     def check_text text
-        return false unless /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.match?(text)
+       return /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.match?(text)? text ? false
     end
     def createEmployee
         puts "What is the employee's name?:"
@@ -12,9 +12,13 @@ module Routines
         employee = Employee.new(name)
 
         while true
+            time_in = ''
             while true
-                puts "Add a time at which #{employee.name} checked in:"
+                puts "Add a time at which #{employee.name} checked in. Use 24hs format in the shape of XX:XX"
                 time = check_text(gets.strip)
+                break if time
+                puts "Invalid time format"
+            end
         end
 
     end
