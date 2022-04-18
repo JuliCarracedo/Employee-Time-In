491f9bc9ccf8bb88f8cd665c9ca3b320ba6a61ee
JuliCarracedo <julicarra@hotmail.com>
2022-04-18 19:36:34 -0300

    Build full data input routine

--- app/script.rb
+++ app/script.rb
@@ -2,4 +2,17 @@ require_relative "./Routines"
 
 include Routines
 
-createEmployee()
+
+employees = []
+
+puts "Welcome. Insert as many employees as you wish."
+while true 
+    employee = createEmployee
+    employees << employee
+    puts "Press any key to continue, or q to quit inserting users"
+    break if gets.strip === "q"
+end
+
+employees.each do |e|
+    print_employee_times(e)
+end
