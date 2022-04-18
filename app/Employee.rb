9d43f15c722e7b439f7930906b48c6818a881b56
JuliCarracedo <julicarra@hotmail.com>
2022-04-18 18:49:34 -0300

    Remove unnecesary logic

--- app/Employee.rb
+++ app/Employee.rb
@@ -25,27 +25,4 @@ class Employee
 
     private 
 
-    def contains? new_times
-        @times.each do |t1|
-            if new_times[0].later_than?(t1[0]) && t1[1].later_than?(new_times[1])
-                return true
-            end
-        end
-        return false
-    end
-
-    def merge new_times
-        @change = false
-        @times.each do |t1|
-            if t1[0].later_than?(new_times[0]) && new_times[1].later_than?(t1[0])
-                t1[0] = new_times[0]
-                @change = true
-            end
-            if t1[1].later_than?(new_times[0]) && new_times[1].later_than?(t1[1])
-                t1[1] = new_times[1]
-                @change = true
-            end           
-        end
-        return @change
-    end
 end
